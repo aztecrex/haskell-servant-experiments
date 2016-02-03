ed687447f33c8c10b697afd2d879775537a44bae
Greg Wiley <gwiley@cj.com>
2016-02-03 08:12:39 -0800

    an app

--- src/WebApp.hs
+++ src/WebApp.hs
@@ -5,3 +5,15 @@ module WebApp where
 
 import Data.Text
 import Servant.API
+
+
+type UserAPI = "users" :> QueryParam "sortby" SortBy :> Get '[JSON] [User]
+          :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] [User] 
+          :<|> "users" :> Capture "userId" Integer :> Get '[JSON] User
+
+data SortBy = Age | Name
+
+data User = User {
+  name :: String,
+  age :: Int
+}
